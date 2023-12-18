a8f9810407fc9d0d8b8d0029c05ab3f52e331200
leejun <leejun.happy@gmail.com>
2023-12-18 23:07:30 +0800

    Revert "feat: home => Token"
    This reverts commit fa2ed74edd327eb5f105f90034fbdbfa17d2fe1c.

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,30 +1,30 @@
 import { View } from '@tarojs/components'
 import { Button } from "@nutui/nutui-react-taro"
 import { useSelector, useDispatch } from 'react-redux'
-import { setToken } from '@/store/modules/user'
+import { increment } from '@/store/modules/home'
 
 import './index.scss'
 
 const Index = () => {
-  const token = useSelector((state: any) => {
+  const counter = useSelector((state: any) => {
     console.log(state)
-    return state.userSlice.token
+    return state.homeReducer.counter
   })
 
   const dispatch = useDispatch()
   const handle = () => {
-    dispatch(setToken(Math.random()))
+    dispatch(increment(1))
   }
 
   return (
     <View className="nutui-react-demo">
       <View className="index">
-        欢迎使用项目。
-        {token}
+        欢迎使用 NutUI React 开发 Taro 多端项目。
+        {counter}
       </View>
       <View className="index">
         <Button type="primary" className="btn" onClick={handle}>
-          Button
+          NutUI React Button
         </Button>
       </View>
     </View>
